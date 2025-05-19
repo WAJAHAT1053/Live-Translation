2ac8cc223179e47f4751827e596f83f0a268896c
Firasat Hussain Mohammed <mfirasathussain@gmail.com>
2025-05-19 18:32:53 -0400

    host powers

--- frontend/pages/room/[roomId].js
+++ frontend/pages/room/[roomId].js
@@ -105,7 +105,7 @@ export default function Room() {
   useEffect(() => {
     const stored = localStorage.getItem('username');
     if (stored) setUsername(stored);
-  }, []);
+  }, []); 
 
   // Get local media with error handling
   useEffect(() => {
